2372ca17b1482c7b5dacb07b637223e5ece9959b
Ashish <ufoundashish@gmail.com>
2019-02-27 00:04:07 +0530

    converted to api code

--- db_functions.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as lite
-import sys
-
-con = lite.connect('paperstuff.db')
-
-with con:
-    cur = con.cursor()
-    cur.execute("CREATE TABLE Paper(PaperName TEXT, Title TEXT, Summary TEXT)")
--- textSummarizer.py
+++ textSummarizer.py
@@ -4,9 +4,19 @@ from operator import itemgetter
 from nltk.corpus import brown, stopwords
 from nltk.cluster.util import cosine_distance
 import warnings
+
 warnings.filterwarnings('ignore')
+stop_words = stopwords.words('english')
+
 
 def sentence_similarity(sent1, sent2, stopwords=None):
+    """
+    returns the sentence similarity between two given sentences
+    :param sent1: sentence 1 - string
+    :param sent2: sentence 2 - string
+    :param stopwords: A list of english stop words
+    :return:
+    """
     if stopwords is None:
         stopwords = []
 
@@ -33,13 +43,9 @@ def sentence_similarity(sent1, sent2, stopwords=None):
     return 1 - cosine_distance(vector1, vector2)
 
 
-stop_words = stopwords.words('english')
-
 def build_similarity_matrix(sentences, stopwords=None):
-
     # Create an empty similarity matrix
     S = np.zeros((len(sentences), len(sentences)))
-
 
     for idx1 in range(len(sentences)):
         for idx2 in range(len(sentences)):
@@ -51,11 +57,12 @@ def build_similarity_matrix(sentences, stopwords=None):
     # normalize the matrix row-wise
 
     for idx in range(len(S)):
-        if(S[idx].sum()==0):
+        if (S[idx].sum() == 0):
             break
         S[idx] /= S[idx].sum()
 
     return S
+
 
 def pagerank(A, eps=0.0001, d=0.85):
     P = np.ones(len(A)) / len(A)
