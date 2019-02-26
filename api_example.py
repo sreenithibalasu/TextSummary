50b6a33561d0deeba831510266e42814c1fb0277
Thomas Ashish Cherian <6967017+PandaWhoCodes@users.noreply.github.com>
2019-02-27 00:20:14 +0530

    Update api_example.py

--- api_example.py
+++ api_example.py
@@ -6,7 +6,7 @@ URL - constant server URL
 request_url - Page that needs to be parsed and summarized.
 """
 
-URL = "http://localhost:5000/summary"
+URL = "http://159.65.36.69:5300/summary"
 request_url = "https://nvie.com/posts/writing-a-cli-in-python-in-under-60-seconds/"
 r = requests.get(URL + "?url=" + request_url)
 print(r.json())
